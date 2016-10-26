78fb76b5617f76b5eb252a2ad22acd8b0653ec57
Sofia Borges <borgessofia22@gmail.com>
2016-10-26 17:04:33 +0200

    Try again button, New search button, CSS, and movie poster

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
     	e.preventDefault()
     	var result = $('input[type="checkbox"]:checked');
     	var genre = result.val();
-    	var url = "https://api.themoviedb.org/3/discover/movie?api_key=c206e5f00d20a4cfe4d4f8fbe1befebd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_genres="
+        var page = Math.floor((Math.random() * 900) + 1);
+    	var url = 'https://api.themoviedb.org/3/discover/movie?api_key=c206e5f00d20a4cfe4d4f8fbe1befebd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page='+ page +'&with_genres='
     	var searchUrl = url + genre
     	if (result.length > 0) {
     		$.ajax({
@@ -20,15 +21,36 @@ $(document).ready(function(){
     })
 
     function showMovie (response){
+        $('.modal-header').hide();
+        total_pages = response.total_pages
     	listMovies = response.results;
-    	var index = 0; 
-    	listMovies.forEach( function(movie) {
-    		console.log(movie.title);
-    	})
+    	recommendation = (listMovies[0]);
+        console.log(recommendation)
+        var imageUrl = 'http://image.tmdb.org/t/p/w185' + recommendation.poster_path
+    	var recommendation_title = $('<h4>').text(recommendation.title);
+        var recommendation_summary = $('<h5>').text(recommendation.overview);
+        $('.movieRecommendation').append($('<img>').attr('src', imageUrl));
+    	$('.movieRecommendation').append(recommendation_title);
+        $('.movieRecommendation').append(recommendation_summary);
+        $('.try-again').show();
+        $('.new-search').show();
+        counter = 0
+        $('.try-again').click(function(e){
+            e.preventDefault()
+            counter++
+            new_movie = listMovies[counter]
+            var imageUrl = 'http://image.tmdb.org/t/p/w185' + new_movie.poster_path
+            var recommendation_title = $('<h4>').text(new_movie.title);
+            var recommendation_summary = $('<h5>').text(new_movie.overview);
+            $('.movieRecommendation').empty();
+            $('.movieRecommendation').append($('<img>').attr('src', imageUrl));
+            $('.movieRecommendation').append(recommendation_title);
+            $('.movieRecommendation').append(recommendation_summary);
+        })
     };
 
     function handleError(error){
-			console.log("error")
-			console.log(error.responseText);
+		console.log("error")
+		console.log(error.responseText);
 	};
 });
