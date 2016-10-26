4837b18a6b5a98cadef90723e01f414712e0864f
Sofia Borges <borgessofia22@gmail.com>
2016-10-26 18:34:13 +0200

    series try again button works

--- app/assets/javascripts/series.js
+++ app/assets/javascripts/series.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     	e.preventDefault()
     	var result = $('input[type="checkbox"]:checked');
     	var genre = result.val();
-        var page = Math.floor((Math.random() * 100) + 1);
+        var page = Math.floor((Math.random() * 10) + 1);
         console.log(page);
     	var url = 'https://api.themoviedb.org/3/discover/tv?api_key=c206e5f00d20a4cfe4d4f8fbe1befebd&language=en-US&sort_by=popularity.desc&page='+ page +'&timezone=America/New_York&with_genres='+ genre +'&include_null_first_air_dates=false'
     	if (result.length > 0) {
@@ -35,19 +35,19 @@ $(document).ready(function(){
      	$('.seriesRecommendation').append(recommendation_summary);
      	$('.try-again').show();
      	$('.new-search').show();
-     //    counter = 0
-     //    $('.try-again').click(function(e){
-     //        e.preventDefault()
-     //        counter++
-     //        new_movie = listMovies[counter]
-     //        var imageUrl = 'http://image.tmdb.org/t/p/w185' + new_movie.poster_path
-     //        var recommendation_title = $('<h4>').text(new_movie.title);
-     //        var recommendation_summary = $('<h5>').text(new_movie.overview);
-     //        $('.movieRecommendation').empty();
-     //        $('.movieRecommendation').append($('<img>').attr('src', imageUrl));
-     //        $('.movieRecommendation').append(recommendation_title);
-     //        $('.movieRecommendation').append(recommendation_summary);
-     //    })
+        counter = 0
+        $('.try-again').click(function(e){
+            e.preventDefault()
+            counter++
+            new_serie = listSeries[counter]
+            var imageUrl = 'http://image.tmdb.org/t/p/w185' + new_serie.poster_path
+            var recommendation_title = $('<h4>').text(new_serie.title);
+            var recommendation_summary = $('<h5>').text(new_serie.overview);
+            $('.seriesRecommendation').empty();
+            $('.seriesRecommendation').append($('<img>').attr('src', imageUrl));
+            $('.seriesRecommendation').append(recommendation_title);
+            $('.seriesRecommendation').append(recommendation_summary);
+        })
     };
 
     function handleError(error){
